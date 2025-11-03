bbb5d30d8e728788e8c0f5ae3d8c0d878f029e33
Harsh Vardhan Soni <sonihv01@gmail.com>
2025-11-03 10:57:26 +0530

    understood layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>header</div>
         {children}
+        <div>footer</div>
       </body>
     </html>
   );
